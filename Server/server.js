bb91dcfc1ce123258cb3e8b811015fe02bc772d0
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-02-24 15:58:40 -0800

    DB Hook and connection with .env file

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connection = require('./utilities/connection');
 
-const port = 3000;
 const app = express();
 
 app.use(cors());
@@ -13,9 +14,12 @@ app.get('/', (req, res) => {
     res.send("Finance Tracker Application.");
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     console.log(req.body);
+    const sql = `SELECT * FROM account_book WHERE book_name = ${req.body.username} AND book_password = ${req.body.password}`;
+    const [rows] = await connection.promise().query(sql);
+    console.log(rows);
     res.send("Token");
 });
 
-app.listen(port, () => console.log(`Server is listening on port http://localhost:${port}/.`));
+app.listen(process.env.PORT, () => console.log(`Server is listening on port http://localhost:${process.env.PORT}/.`));
