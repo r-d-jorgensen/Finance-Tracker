d50e7072b5cd773969fd0353577d3cff6c4e39d6
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-02-28 19:43:48 -0800

    regex to allow special char

--- Server/users/user.dataSchemas.js
+++ Server/users/user.dataSchemas.js
@@ -4,8 +4,7 @@ export {
     userSchema,
 };
 
-// TODO: currently only alphaNum.. should allow special chars for password security
 let userSchema = object({
-    username: string().matches('^[a-zA-Z0-9]').min(5).required(),
-    password: string().matches('^[a-zA-Z0-9]').min(3).required()
+    username: string().matches('^[a-zA-Z0-9!@#$%^&*?]').min(5).required(),
+    password: string().matches('^[a-zA-Z0-9!@#$%^&*?]').min(3).required()
 });
