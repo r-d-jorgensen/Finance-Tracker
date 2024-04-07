ef5ed37a4a890039093bf968cc2f627ef5432b96
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-04-07 08:12:09 -0700

    account book structure

--- accountBook/accountBook.controller.js
+++ accountBook/accountBook.controller.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import createAccountBook from './accountBook.create.js';
+import deleteAccountBook from './accountBook.delete.js';
 import getAccountBook from './accountBook.get.js';
+import updateAccountBook from './accountBook.update.js';
 const router = express.Router();
 
 // Record routes
 router.post('/createAccountBook', createAccountBookControler);
+router.post('/deleteAccountBook', deleteAccountBookControler);
 router.post('/getAccountBook', getAccountBookControler);
+router.post('/updateAccountBook', updateAccountBookControler);
 
 function createAccountBookControler(req, res, next) {
     createAccountBook(req.body)
@@ -13,8 +17,20 @@ function createAccountBookControler(req, res, next) {
         .catch(next);
 }
 
+function deleteAccountBookControler(req, res, next) {
+    deleteAccountBook(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
 function getAccountBookControler(req, res, next) {
     getAccountBook(req.body)
+        .then(user => res.json(user))
+        .catch(next);
+}
+
+function updateAccountBookControler(req, res, next) {
+    updateAccountBook(req.body)
         .then(user => res.json(user))
         .catch(next);
 }
