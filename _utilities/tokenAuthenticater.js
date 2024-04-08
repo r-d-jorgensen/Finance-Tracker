e3357876587eb15cd229fa889bf981a29febf780
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-04-08 13:09:20 -0700

    path update

--- _utilities/tokenAuthenticater.js
+++ _utilities/tokenAuthenticater.js
@@ -6,8 +6,8 @@ function tokenAuthenicator() {
     const secret = process.env.TOKEN_SECRET;
     return expressjwt({ secret, algorithms: ['HS256'] }).unless({
         path: [
-            '/users/loginUser',
-            '/users/createUser'
+            '/user/loginUser',
+            '/user/createUser'
         ]
     });
 }
