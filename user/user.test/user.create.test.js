04880211f7c70995de42b00cf2f6a1d0f2aadf2f
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-04-16 04:20:02 -0700

    jest env

--- user/user.test/user.create.test.js
+++ user/user.test/user.create.test.js
@@ -1,26 +1,19 @@
+import { randomInt } from "crypto";
 import createUser from "../user.create";
 
 describe('validateEmail function', () => {
     const testUser = {
-        "username": "David2131",
-        "password": "stuff",
+        "username": "TestUser" + randomInt(1000000000),
+        "password": "testPassword" + randomInt(1000000000),
         "email": "richard.david.jorgensen@gmail.com"
     };
   
-    beforeEach(() => {
-      
-    });
-  
-    afterEach(() => {
-      
-    });
-  
     it('should create and return a new user', async () => {
         const result = await createUser(testUser);
-    
+        
         expect(result && typeof result === 'object').toBe(true)
         expect(typeof result.user_id).toBe("number");
-        expect(typeof result.username).toBe("token");
+        expect(typeof result.username).toBe("string");
         expect(result.username).toBe(testUser.username);
         expect(typeof result.token).toBe("string");
     });
