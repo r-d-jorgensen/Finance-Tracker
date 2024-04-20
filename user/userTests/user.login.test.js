c47f8f6fa76538149f03be2f4e3289f6cb4839e5
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2024-04-20 15:30:11 -0700

    user login tests

---
+++ user/userTests/user.login.test.js
@@ -0,0 +1,117 @@
+import { randomInt } from "crypto";
+import supertest from "supertest";
+import connectionPool from '../../_utilities/connection.js';
+import app from "../../server.js";
+
+describe('Login User - /user/loginUser', () => {
+    const testUser = {
+        username: "TestUser" + randomInt(1000000000),
+        password: "testPassword" + randomInt(1000000000),
+    };
+
+    beforeAll(async () => {
+        await supertest(app)
+        .post('/user/createUser')
+        .set('Accept', 'application/json')
+        .send({
+            username: testUser.username,
+            password: testUser.password,
+            email: "bigstuff@gmail.com",
+        });
+    });
+  
+    it('should return new user id and auth token', async () => {
+        const res = await supertest(app)
+            .post('/user/loginUser')
+            .set('Accept', 'application/json')
+            .send(testUser);
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.headers['content-type']).toContain('application/json');
+        expect(res.body).toEqual(
+            expect.objectContaining({
+                user_id: expect.any(Number),
+                username: testUser.username,
+                token: expect.any(String),
+            }),
+        );
+
+        const sqlquery = `SELECT * FROM users WHERE user_id = ?`;
+        const user = (await connectionPool.execute(sqlquery, [res.body.user_id]))[0];
+        expect(user.length).toEqual(1);
+        expect(user[0].username).toEqual(testUser.username);
+    });
+
+    it('should return error of not enough characters for username', async () => {
+        testUser.username = "fs";
+        const res = await supertest(app)
+            .post('/user/loginUser')
+            .set('Accept', 'application/json')
+            .send(testUser);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.headers['content-type']).toContain('application/json');
+        expect(res.body).toEqual(
+            expect.objectContaining({
+                name: 'ValidationError',
+                message: 'Data did not match allowed structure',
+                inValidEntries: [ 'username must be at least 5 characters' ]
+            }),
+        );
+    });
+
+    it('should return error of bad characters for username', async () => {
+        testUser.username = "fs|=";
+        const res = await supertest(app)
+            .post('/user/loginUser')
+            .set('Accept', 'application/json')
+            .send(testUser);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.headers['content-type']).toContain('application/json');
+        expect(res.body).toEqual(
+            expect.objectContaining({
+                name: 'ValidationError',
+                message: 'Data did not match allowed structure',
+                inValidEntries: [ "username must match the following: \"/^[a-zA-Z0-9!@#$%^&*?]+$/\"" ]
+            }),
+        );
+    });
+
+    it('should return error of not enough characters for password', async () => {
+        testUser.password = "fs";
+        const res = await supertest(app)
+            .post('/user/loginUser')
+            .set('Accept', 'application/json')
+            .send(testUser);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.headers['content-type']).toContain('application/json');
+        expect(res.body).toEqual(
+            expect.objectContaining({
+                name: 'ValidationError',
+                message: 'Data did not match allowed structure',
+                inValidEntries: [ 'password must be at least 5 characters' ]
+            }),
+        );
+    });
+
+    it('should return error of bad characters for password', async () => {
+        testUser.password = "fs|=";
+        const res = await supertest(app)
+            .post('/user/loginUser')
+            .set('Accept', 'application/json')
+            .send(testUser);
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.headers['content-type']).toContain('application/json');
+        expect(res.body).toEqual(
+            expect.objectContaining({
+                name: 'ValidationError',
+                message: 'Data did not match allowed structure',
+                inValidEntries: [ "password must match the following: \"/^[a-zA-Z0-9!@#$%^&*?]+$/\"" ]
+            }),
+        );
+    });
+});
+
